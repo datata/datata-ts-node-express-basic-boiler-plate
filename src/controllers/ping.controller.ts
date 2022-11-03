fba98ec51e5c8ceae450e21421aff29a058e1e01
datata <dani@danitarazona.dev>
2022-11-03 17:17:10 +0100

    feat: apiResponse interface added

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express'
 
-export const ping = (_req: Request, res: Response) => {
-  return res.status(200).json(
-    {
-      success: true,
-      message: 'pong'
-    }
-  )
+export interface apiResponse {
+  success: Boolean
+  message: String
+}
+export const ping = (_req: Request, res: Response): Response<JSON> => {
+  const result: apiResponse = {
+    success: true,
+    message: 'pong'
+  }
+
+  return res.status(200).json(result);
 }
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT ?? 3000
 
 app.use('/api', pingRoutes)
 
